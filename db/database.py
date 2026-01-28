0b44cc9820029d731b780902aa9f538fd2c14074
alicemary7 <alice.mary.zestober@gmail.com>
2026-01-28 17:51:03 +0530

    db url changed

--- db/database.py
+++ db/database.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import declarative_base, sessionmaker
 
-db_url = "postgresql+psycopg2://postgres:AcademyRootPassword@localhost:5432/fullstack_project"
+db_url = "postgresql://e_commerce_website_m16e_user:6KBd02SB5rfwe701VH2YaSJOCtUMzfE5@dpg-d5scavi4d50c73d9f9o0-a.virginia-postgres.render.com/e_commerce_website_m16e"
 
 engine = create_engine(db_url)
 
